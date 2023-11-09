245f15f1f5bbdd0e272ca9d5d3b8470a753aa4d9
Alzavio <luiz.matthew187@gmail.com>
2023-11-08 21:49:01 -0800

    Last javadoc

--- src/NSCMediaUtils.java
+++ src/NSCMediaUtils.java
@@ -1,5 +1,7 @@
 public class NSCMediaUtils {
     /**
+     * Takes a media item, and calls their Stringify method which prints all of their variables
+     *
      * @param media Book, DVD, and Magazine
      */
     public static void printMediaDetails(Media media) {
